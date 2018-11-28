b09f66954f6da9c36f8f4fb41e9bc9b5b663688f
Cameron <cameron.39@gmail.com>
2018-11-27 21:47:41 -0600

    Removed drawable import

--- app/src/main/java/com/example/cameron/heartlessgravity/TheGame.java
+++ app/src/main/java/com/example/cameron/heartlessgravity/TheGame.java
@@ -9,7 +9,6 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
